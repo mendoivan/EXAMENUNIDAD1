5ebccfda3f9767618c5513bb71addfc28e471876
Mendoza <20233tn038@utez.edu.mx>
2024-05-24 11:33:41 -0600

    rama mari

---
+++ Mari.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Mari {
+    public static void main (String[]args){
+        System.out.println("!HOLA MUNOD¡, Maria");
+    }
+}
---
+++ Pedro.java
@@ -0,0 +1,7 @@
+package org.example;
+
+public class Pedro {
+    public static void main(String[] args) {
+        System.out.println("!HOLA MUNDO¡, Pedro");
+    }
+}
